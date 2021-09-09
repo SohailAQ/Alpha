c1183f9613caf5693ee77cd909e386b622550135
SohailAQ <sohail.developes@gmail.com>
2021-09-09 22:50:14 +0530

    CRUD Operations

---
+++ product/views.py
@@ -0,0 +1,37 @@
+from django.urls import reverse_lazy
+from django.views.generic import (ListView,
+                                  CreateView,
+                                  DetailView,
+                                  UpdateView,
+                                  DeleteView)
+
+from product.models import Product
+
+
+class ProductListView(ListView):
+    model = Product
+    context_object_name = 'product_list'
+    template_name = 'product_list.html'
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    template_name = 'product/product_form.html'
+    fields = ['name', 'manufacturer', 'price', 'quantity']
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    context_object_name = 'product'
+    template_name = 'product/product_detail.html'
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = ['name', 'manufacturer', 'price', 'quantity']
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    context_object_name = 'product'
+    success_url = reverse_lazy('product-list')
